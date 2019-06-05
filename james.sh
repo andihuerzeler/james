9fff61496329e792b404f6187484865852ee182b
ahousseini <admin@anver-mac.intra.anver.ch>
2019-06-05 21:46:30 +0200

    Update james.sh

--- james.sh
+++ james.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls "manifests/"*.json | while read manifests; do
+ls "james/manifests/"*.json | while read manifests; do
 
   jps=$(jq -r '.url' "${manifests}")
   jps_purged=$(jq -r '.url' "${manifests}" | sed 's~http[s]*://~~g')
@@ -10,7 +10,7 @@ ls "manifests/"*.json | while read manifests; do
 
     echo "==> JPS '${jps}' is available"
 
-    ls -d "templates/"* | while read templates; do
+    ls -d "james/templates/"* | while read templates; do
 
       templates=$(basename ${templates})
       templates_purged=$(basename ${templates} | cut -c 4-)
@@ -24,14 +24,14 @@ ls "manifests/"*.json | while read manifests; do
           object_purged=$(echo "${object}" | sed -e 's/ /%20/g' 2>/dev/null)
 
           if [ "$(curl -sL -H "authorization: Basic ${credentials}" -w "%{http_code}" "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -o /dev/null)" == "200" ]; then
-            if [ -s "templates/${templates}/${object}.xml" ]; then
+            if [ -s "james/templates/${templates}/${object}.xml" ]; then
               echo "[$n] Make PUT request on resource '${object}' to '${jps_purged}'"
-              curl -w "\n" -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -X PUT
+              curl -w "\n" -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "james/templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -X PUT
             fi
           elif [ "$(curl -sL -H "authorization: Basic ${credentials}" -w "%{http_code}" "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -o /dev/null)" == "404" ]; then
-            if [ -s "templates/${templates}/${object}.xml" ]; then
+            if [ -s "james/templates/${templates}/${object}.xml" ]; then
               echo "[$n] Make POST request on resource '${object}' to '${jps_purged}'"
-              curl -w "\n" -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/id/0" -X POST
+              curl -w "\n" -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "james/templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/id/0" -X POST
             fi
           fi
 
