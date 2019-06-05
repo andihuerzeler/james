e6224b6dc08065226c7e74e68b6794146de62a4a
ahousseini <admin@anver-mac.intra.anver.ch>
2019-06-05 21:40:54 +0200

    Update james.sh

--- james.sh
+++ james.sh
@@ -1,16 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 
-ls "james/manifests/"*.json | while read manifests; do
+ls "manifests/"*.json | while read manifests; do
 
   jps=$(jq -r '.url' "${manifests}")
   jps_purged=$(jq -r '.url' "${manifests}" | sed 's~http[s]*://~~g')
   echo "==> Process manifest '${manifests}'"
 
-  if [ "$(/usr/bin/curl -sL -w "%{http_code}" "${jps}/healthCheck.html" -o /dev/null)" == "200" ]; then
+  if [ "$(curl -sL -w "%{http_code}" "${jps}/healthCheck.html" -o /dev/null)" == "200" ]; then
 
     echo "==> JPS '${jps}' is available"
 
-    ls -d "james/templates/"* | while read templates; do
+    ls -d "templates/"* | while read templates; do
 
       templates=$(basename ${templates})
       templates_purged=$(basename ${templates} | cut -c 4-)
@@ -23,15 +23,15 @@ ls "james/manifests/"*.json | while read manifests; do
           object=$(jq -r --arg t "${templates_purged}" --arg n "${n}" '.[$t][$n | tonumber]' "${manifests}")
           object_purged=$(echo "${object}" | sed -e 's/ /%20/g' 2>/dev/null)
 
-          if [ "$(/usr/bin/curl -sL -H "authorization: Basic ${credentials}" -w "%{http_code}" "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -o /dev/null)" == "200" ]; then
-            if [ -s "james/templates/${templates}/${object}.xml" ]; then
-              echo " [$n] Make PUT request on resource '${object}' to '${jps_purged}'"
-              curl -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "james/templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -X PUT > /dev/null
+          if [ "$(curl -sL -H "authorization: Basic ${credentials}" -w "%{http_code}" "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -o /dev/null)" == "200" ]; then
+            if [ -s "templates/${templates}/${object}.xml" ]; then
+              echo "[$n] Make PUT request on resource '${object}' to '${jps_purged}'"
+              curl -w "\n" -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -X PUT
             fi
-          elif [ "$(/usr/bin/curl -sL -H "authorization: Basic ${credentials}" -w "%{http_code}" "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -o /dev/null)" == "404" ]; then
-            if [ -s "james/templates/${templates}/${object}.xml" ]; then
-              echo " [$n] Make POST request on resource '${object}' to '${jps_purged}'"
-              curl -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "james/templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/id/0" -X POST > /dev/null
+          elif [ "$(curl -sL -H "authorization: Basic ${credentials}" -w "%{http_code}" "${jps}/JSSResource/${templates_purged}/name/${object_purged}" -o /dev/null)" == "404" ]; then
+            if [ -s "templates/${templates}/${object}.xml" ]; then
+              echo "[$n] Make POST request on resource '${object}' to '${jps_purged}'"
+              curl -w "\n" -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "templates/${templates}/${object}.xml" -s "${jps}/JSSResource/${templates_purged}/id/0" -X POST
             fi
           fi
 
