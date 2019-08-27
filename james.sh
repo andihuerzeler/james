78c39f5e32dc3b7b6b0a7747c9a7ceb0784054fe
ahousseini <contact@anver.ch>
2019-08-27 15:38:56 +0200

    Update test

--- james.sh
+++ james.sh
@@ -62,8 +62,8 @@ find "james/manifests/"*.json | while read -r manifests; do
                   echo "[$n] Add resource '${templates_purged}/${object}' on '${jps_purged}'"
                   curl -s -o "/dev/null" --show-error -H "authorization: Basic ${credentials}" -H "content-type: application/xml" -T "james/templates/${templates}/${object}.xml" "${jps}/JSSResource/${templates_purged}/id/0" -X POST
 
-                  if [ -s "james/icons/${object}.png" ]; then
-                    icon="james/icons/${object}.png"
+                  if [ -s "james/icons/${object/ Self Service/}.png" ]; then
+                    icon="james/icons/${object/ Self Service/}.png"
                     object_id=$(curl -s -H "authorization: Basic ${credentials}" -H "accept: application/xml" -X "GET" "${jps}/JSSResource/policies/name/${object_purged}" | xmllint --xpath "/policy/general/id/text()" -)
                     if [ -z "$(curl -s -H "authorization: Basic ${credentials}" "${jps}/JSSResource/policies/name/${object_purged}" | xmllint --xpath "/policy/self_service/self_service_icon/filename/text()" - 2>/dev/null)" ]; then
                       echo "Add Self Service icon '${icon}' to policy '${object_purged}'"
